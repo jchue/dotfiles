c8c5c0772c4bf552f3d669a6fa8cf5cc047a1d8e
jchue <5141427+jchue@users.noreply.github.com>
2020-09-08 13:33:19 -0700

    Replace gitignore with symbolic links

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Spicetify
+ln -sT $dotfiles/.config/spicetify/Extensions $HOME/.config/spicetify/Extensions
+ln -sT $dotfiles/.config/spicetify/Themes $HOME/.config/spicetify/Themes
+ln -si $dotfiles/.config/spicetify/config.ini $HOME/.config/spicetify/config.ini
+
+spicetify backup apply
+
+# Firefox
+for directory in $(ls -a .mozilla/firefox/); do
+    if [[ $directory == *".default-release"* ]]; then
+        firefox_profile=$directory
+    fi
+done
+
+ln -sT $dotfiles/.mozilla/firefox/.default-release/chrome $HOME/.mozilla/firefox/$firefox_profile/chrome
+
+# Vim
+ln -sT $dotfiles/.vim/colors $HOME/.vim/colors

de37e5b50f0799c356a1cc4e3b2d051d90437e99
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2020-10-27 22:51:22 -0700

    Update fonts in polybar; clean up bar content

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar --config=$HOME/.config/polybar/config.ini bar1 >>/tmp/polybar1.log 2>&1 & disown
+polybar --config=$HOME/.config/polybar/config.ini -r bar1 >>/tmp/polybar1.log 2>&1 & disown
 
 echo "Bars launched..."
